6205ed96a6375c662ac03465bde7fbf4a546ff28
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-04-21 19:58:10 +0530

    Time: 427 ms (7.63%), Space: 81.5 MB (10.34%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int siz=nums.size();
+        set <int> s(nums.begin(),nums.end());
+        cout<<siz<<endl<<s.size();
+        if((nums.size()-s.size())==0){
+            return false;
+        }
+        else{
+            for(int i=0;i<siz;i++){
+            for(int j=i+1;j<siz;j++){
+                if((abs(i-j)<=k) && (nums[i]==nums[j])){
+                    return 1;
+                    // break;
+                }
+        }
+            }
+        }
+//         for(int i=0;i<siz;i++){
+//             for(int j=i+1;j<siz;j++){
+//                 if((abs(i-j)<=k) && (nums[i]==nums[j])){
+//                     return 1;
+//                     // break;
+//                 }
+//                 // else{
+//                 //     return 0;
+//                 //     // break;
+//                 // }
+//             }
+            
+//         }
+//         return 0;
+        return 0;
+    }
+};
