642ab5602167df204ff8da917c125ec1a53cbeab
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-05-06 11:21:25 +0530

    Time: 28 ms (63.68%), Space: 10.8 MB (49.54%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector <pair<char,int>> p;
+        string ans;
+        for (auto c: s){
+            if(p.empty() || p.back().first!=c){
+                p.push_back({c,1});
+            }
+            else{
+                p.back().second++;
+                if( p.back().second==k){
+                    p.pop_back();
+                }
+            }
+        }
+        for(auto ele:p){
+            ans.append(ele.second,ele.first);
+        }
+        return ans;
+        
+    }
+};
+
+// string ans;
+//         int c=0;
+//         for(int i=0;i<s.size();i++){
+//             if(ans.length()!=0 || ans[ans.length()-1]==s[i]){
+//                 ans.push_back(s[i]);
+//                 c++;
+//                 if(ans.length()!=0 ||  (c==k)){
+//                     for(int j=1;j<=k;i++){
+//                         ans.pop_back();
+//                     }
+//                     c=0;
+//                 }
+//             }
+//             else{
+//                 ans.push_back(s[i]);
+//             }
+//         }
+        
+//         return ans;
