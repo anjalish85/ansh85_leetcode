62009c6aeab11d40a0d9ca9308342370f0b983d5
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-05-06 10:01:11 +0530

    Time: 11 ms (98.43%), Space: 11.1 MB (51.45%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack <char> c;
+        string ans;
+        
+        for(int i=0;i<s.size();i++){
+            if(ans.length()!=0 && s[i]==ans[ans.length()-1]){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+        }
+        
+//         while(!c.empty()){
+//             ans=ans+c.top();
+//             c.pop();
+//         }
+        
+        // revers/=e(ans.begin(),ans.end());
+        return ans;
+        // return c;
+    }
+};
