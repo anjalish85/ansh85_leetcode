9085a22ef36cab96ea5ed68e0daf34a6789626be
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-05-09 09:23:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.74%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,7 +3,9 @@ public:
     bool isPowerOfTwo(int n) {
         
         if(n==0) return false;
-        while(n%2==0) n/=2;
+        while(n%2==0) 
+            n/=2;
+        //let's say number iis 5 therefore while loop will be terminated and thus 5==1 will return 0;
         return n==1;
     }
 };
