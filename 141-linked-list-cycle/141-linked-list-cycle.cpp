11716ab82298b6aeec6c05e54f6c71eafd48c939
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-05-22 19:21:13 +0530

    Time: 20 ms (25.44%), Space: 7.8 MB (96.16%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -11,11 +11,12 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* f=head;
         ListNode* s=head;
-        while(f!=NULL && f->next!=NULL){
-            s=s->next;
-            f=f->next->next;
-            if(s==f) return 1;
-        }
+       while (f!=NULL && f->next!=NULL){
+           s=s->next;
+           f=f->next->next;
+           if(s==f)
+               return 1;
+       }
         return 0;
     }
 };
