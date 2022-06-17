0b781889933969d1805a08ef6d593a17cf2e1281
Anjali Shaw <65438703+cool-cupid@users.noreply.github.com>
2022-06-17 08:25:19 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (61.12%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size() - 1;
+    int n = digits.size() - 1;
       
       for (int i = n; i >= 0; --i) { 
+          //case for successive 9
         if (digits[i] == 9) {  
           digits[i] = 0;
+            //normal case
         } else {  
           digits[i] += 1;
           return digits;
         }
       }
-        digits.push_back(0);
-        digits[0]=1;
-        return digits;
+    //givib=ving the finishing for 9999 cases
+    digits.push_back(0);
+    digits[0]=1;
+    return digits;
     }
 };
