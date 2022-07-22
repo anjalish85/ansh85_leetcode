9b78bc2e01c7ea4a03b0ac438e076ef05d6e6ff4
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-22 11:55:46 +0530

    Time: 15 ms (23.79%), Space: 6 MB (24.83%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -4,12 +4,12 @@ public:
         int k=1;
         int c=0;
         while(n>=0){
+            
             if(n>=k){
                 c++;
             }
             n=n-k;
             k++;
-            // c++;
         }
         return c;
     }
