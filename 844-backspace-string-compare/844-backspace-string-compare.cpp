022ff39a4fb4f0489adfe474d6f18898e8761574
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-27 08:55:37 +0530

    Time: 6 ms (17.32%), Space: 6.3 MB (44.28%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,64 +1,23 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack <char> s1,s2;
+        stack <char> s1,t1;
+        
         for(int i=0;i<s.size();i++){
-            if(s[i] == '#' && !s1.empty()){
+            if(s[i]=='#' and !s1.empty())
                 s1.pop();
-            }
-            if(s[i] != '#'){
+            if(s[i]!='#')
                 s1.push(s[i]);
-            }
         }
-        
         for(int i=0;i<t.size();i++){
-            if(t[i] == '#' && !s2.empty()){
-                s2.pop();
-            }
-            if(t[i] != '#'){
-                s2.push(t[i]);
-            }
+            if(t[i]=='#' and !t1.empty())
+                t1.pop();
+            if(t[i]!='#')
+                t1.push(t[i]);
         }
         
-        if(s1==s2)
-            return 1;
-        return 0;
+        // cout<<s1<<" "<<t1;
+        
+        return (s1==t1);
     }
 };
-
-
-
-
-// vector <char> f1,f2;
-        
-//         if(s[0]=='#'){
-//             f1.clear();//break;//continue;
-//         }
-//         else{
-//                 for(int i=0;i<s.size();i++){
-//                 if(s[i]=='#'){
-//                     f1.pop_back();
-//                 }
-//                 else{
-//                     f1.push_back(s[i]);
-//                 }
-//             }
-//         }
-        
-//         if(t[0]=='#'){
-//             f2.clear();//continue;//f2==0;
-//         }
-//         else{
-//                 for(int i=0;i<t.size();i++){
-//                 if(t[i]=='#'){
-//                     f2.pop_back();
-//                 }
-//                 else{
-//                     f2.push_back(t[i]);
-//                 }
-//             }
-//         }
-                
-//         if(f1==f2)
-//             return 1;
-//         return 0;
