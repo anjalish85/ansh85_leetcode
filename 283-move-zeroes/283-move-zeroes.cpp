2561211191a0ac55422afa67f51c399861a87842
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-30 21:33:05 +0530

    Time: 28 ms (80.62%), Space: 19.2 MB (57.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,11 +3,13 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j=0;
         int c=0;
+        //just shifting the non-zero values to the front end
         for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0){
                 nums[j++]=nums[i];
             }
         }
+        //using the same index counter to fill zeroes as j had the value of index till which non-zero values were obtained in nums after shifting 
         for(; j<nums.size(); j++){
             nums[j]=0;
         }
