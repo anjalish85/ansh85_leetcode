c3cf39288fc672101498aef93a658290df4e7766
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-30 21:31:24 +0530

    Time: 19 ms (97.62%), Space: 19.3 MB (57.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int j=0;
-        for(int i=0;i<nums.size();i++){
+        int c=0;
+        for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0){
-                // nums.erase(nums.begin()+i);
-                // nums.push_back(0);
-                nums[j]=nums[i];
-                j++;
+                nums[j++]=nums[i];
             }
         }
-        for(;j<nums.size();j++){
+        for(; j<nums.size(); j++){
             nums[j]=0;
         }
     }
