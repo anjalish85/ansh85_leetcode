c6020df6d30b323aacdee66f002de547af19bb9b
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-30 21:35:44 +0530

    Time: 8 ms (96.61%), Space: 10.8 MB (54.06%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector <int> ans;
-        unordered_map <int,int> m;
+        unordered_map <int, int> m;
         
         for(int i=0;i<nums.size();i++){
-            if(m.find(target-nums[i]) != m.end()){
+            //checking if target-current value is availabe in array to be used.
+            //step-1
+            if(m.find(target-nums[i])!=m.end()){
+                //if available then simply produccing answer
                 ans.push_back(m[target-nums[i]]);
                 ans.push_back(i);
                 return ans;
             }
+            //if not then storing the values and it's index so that it can be further used to check as in step-1
             m[nums[i]]=i;
         }
         
