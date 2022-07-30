2ea3bb711796a673df5a48f3a5c27e96e5b2df7e
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-07-30 21:48:59 +0530

    Time: 27 ms (24.69%), Space: 15.5 MB (75.51%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -7,10 +7,14 @@ public:
         int h=numbers.size()-1;
         
         while(l<h){
+            //checking if sum is present or not
             if(numbers[l]+numbers[h]==target)
+                //incrementing before returning as qn is 1-indexed
                 return {++l, ++h};
+            //if number wanted is greater than what's needed then choosing the lesser value present before it
             else if(numbers[l]+numbers[h]>target)
                 h--;
+            //if number wanted is lesser than what's needed then choosing the greater value present just after it
             else
                 l++;
         }
