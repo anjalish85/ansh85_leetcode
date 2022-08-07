fe1c3e9ae540e0a970da9c1fc99ede9c0260e6aa
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-07 21:02:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (51.66%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -7,6 +7,7 @@ public:
         v[1]=1;
         v[2]=1;
         
+        //storing the values in array format so that we can use it for defining next index
         for(int i=3; i<38; i++){
             v[i]=(v[i-1]+v[i-2]+v[i-3]);
         }
