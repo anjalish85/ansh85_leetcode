cc9bfebeb5429fb370bc77c18d70260b0cb2b19a
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-12 20:45:12 +0530

    Time: 17 ms (29.23%), Space: 14.3 MB (5.57%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //writing funcion to find first occurence
     int fp(vector <int> nums, int target){
         int l=0;
         int r=nums.size()-1;
@@ -9,6 +10,7 @@ public:
             int mid=(l+r)/2;
             if(nums[mid]==target){
                 ans=mid;
+                //initiating rear end point to find the first occurence
                 r=mid-1;
             }
             else if(nums[mid]<target)
@@ -19,6 +21,7 @@ public:
         return ans;
     }
     
+    //writing funcion to find last occurence
     int lp(vector <int> nums, int target){
         int l=0;
         int r=nums.size()-1;
@@ -28,6 +31,7 @@ public:
             int mid=(l+r)/2;
             if(nums[mid]==target){
                 ans=mid;
+                //initiating front end point to find the last occurence
                 l=mid+1;
             }
             else if(nums[mid]<target)
