6927a5342611ea6dfddc4cea69208d10b754d02e
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-21 20:31:04 +0530

    Time: 14 ms (71.20%), Space: 10.3 MB (24.78%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -3,9 +3,13 @@ public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         vector<int> nums2=nums;
         vector<int> ans;
+        // using sorted array so that it will be easy to calculate number of elements lesser tthan it
         sort(nums2.begin(), nums2.end());
         
         for(int i=0; i<nums.size(); i++){
+            //find will return the iterator to element
+            //distance will return index
+            //pushing the index value to return it
             ans.push_back(distance(nums2.begin(), find(nums2.begin(), nums2.end(), nums[i])));
         }
         
