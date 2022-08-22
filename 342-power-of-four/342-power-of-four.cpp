cc646fc55d53f8c09e7b7670ff2c0c3b65000c3e
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-22 08:41:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        //for zero
         if(n==0)
             return 0;
-        while(n%4==0 && n>0){
+        //checking if number is divisible by four or not 
+        while(n%4==0){
             n=n/4;
         }
         
