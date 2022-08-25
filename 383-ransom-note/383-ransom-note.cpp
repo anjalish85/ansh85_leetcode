f0ad0c7cdcfdfc3da0762958b2bb713eff9987fd
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-25 08:30:51 +0530

    Time: 27 ms (49.42%), Space: 8.7 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map <char, int> mp;
-        
+        //storing the character frequency
         for(auto ch: magazine){
             mp[ch]++;
         }
-        
+        //matching it with ransomNote's frequency
         for(auto ch: ransomNote){
             if(mp[ch]==0)
                 return 0;
