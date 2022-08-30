9330cdfaa73352fe69ba9187609e620df59381af
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-08-30 09:53:20 +0530

    Time: 6 ms (23.35%), Space: 7.2 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
+    
     void rotate(vector<vector<int>>& matrix) {
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<i; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        
+//         finding transpose
+       for(int i=0; i<matrix.size(); i++){
+           for(int j=0; j<i; j++){
+               swap(matrix[i][j], matrix[j][i]);
+           }
+       }
+//         reversing each row
         for(int i=0; i<matrix.size(); i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
