7a816b21fbc0f764b67a50ffdf4092373010f307
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2022-09-07 10:32:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (12.01%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string tobin(int num){
+        string s="";
+        while(num>0){
+            if(num%2==0)
+                s+='0';
+            else
+                s+='1';
+            num/=2;
+        }
+        return s;
+    }
+    
+    int findComplement(int num) {
+        
+        string s=tobin(num);
+        cout<<s;
+        int sum=0;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='0')
+                sum+=pow(2, i);
+        }
+        return sum;
+    }
+};
